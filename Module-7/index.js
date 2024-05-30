7b0907c9b8bcb9565954e2e001943a6ee2c2fb93
marjanhasan <moonmarjanhasan@gmail.com>
2024-05-30 08:53:04 +0600

    module 7 done

---
+++ Module-7/index.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const cors = require("cors");
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+//********************* MONGODB installation code
+
+const uri =
+  "mongodb+srv://moonmarjanhasan:zcOeEfHJ3C179n5x@cluster0.i8uts9o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    // **************** database creation
+    const userDB = client.db("userDB");
+    const userCollection = userDB.collection("user_collection");
+
+    app.post("/user", async (req, res) => {
+      const user = req.body;
+      const result = await userCollection.insertOne(user);
+      console.log(result);
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+  }
+}
+run().catch(console.dir);
+
+//**********************
+
+app.get("/", (req, res) => {
+  res.send({ message: "Hello World!" });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+/*
+moonmarjanhasan
+zcOeEfHJ3C179n5x
+
+*/
